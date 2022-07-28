bc2e97152dcc186449ab239e33a8054111527ccf
Andrew Lu <steinernein@gmail.com>
2022-07-28 12:21:54 +0000

    0ee12f4 Add return outside of function. (#597)
    https://github.com/BuilderIO/mitosis/commit/0ee12f45507cb112d4fe7157845c55980b833068

--- dist/src/helpers/babel-transform.js
+++ dist/src/helpers/babel-transform.js
@@ -44,6 +44,7 @@ var babelTransform = function (code, visitor) {
         configFile: false,
         babelrc: false,
         presets: [[tsPreset, { isTSX: true, allExtensions: true }]],
+        parserOpts: { allowReturnOutsideFunction: true },
         plugins: __spreadArray([[decorators, { legacy: true }], jsxPlugin], (visitor ? [function () { return ({ visitor: visitor }); }] : []), true),
     });
 };
