8b72578b2a0402bf87fe6f223dac5888099ee5ef
Steve Sewell <steve@builder.io>
2022-08-11 23:36:09 +0000

    3e0a871 add a test for sub component file format
    https://github.com/BuilderIO/mitosis/commit/3e0a8714d577a3008a238ae4a8d6b84c44400ec6

--- dist/src/__tests__/marko.test.js
+++ dist/src/__tests__/marko.test.js
@@ -1,6 +1,7 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 var marko_1 = require("../generators/marko");
+var jsx_1 = require("../parsers/jsx");
 var shared_1 = require("./shared");
 describe('Marko format', function () {
     var exampleCode = "\n    <div>\n      <if(input.foo === 'bar')>\n        Hello\n      </if>\n      <for|items| of=(some.long ? expression : yo)>\n        World\n      </for>\n      <input \n        placeholder=\"Hello world...\"\n        value=(this.state.name) \n        on-click=(event => {\n          console.log('hello', \"world\")\n        })\n        on-input=(event => this.state.name = event.target.value) /> \n        \n        Hello! I can run in React, Vue, Solid, or Liquid!\n    </div>";
@@ -11,4 +12,8 @@ describe('Marko format', function () {
 });
 describe('Marko', function () {
     (0, shared_1.runTestsForTarget)('marko', (0, marko_1.componentToMarko)());
+    var exampleSubComponent = "\n    import SubComponent from './foo.lite'\n    export default function Example() {\n      return <SubComponent />\n    }\n  ";
+    var component = (0, jsx_1.parseJsx)(exampleSubComponent);
+    var markoString = (0, marko_1.componentToMarko)()({ component: component });
+    expect(markoString).toMatchSnapshot();
 });
