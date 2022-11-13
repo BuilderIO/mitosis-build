1d1cfdb0e74848cc94544c8ca547447cb9686f8d
Sami Jaber <jabersami@gmail.com>
2022-11-13 14:39:53 +0000

    eb428bd Refactor/improve types (#904)
    https://github.com/BuilderIO/mitosis/commit/eb428bd110ee3cad1685caa26818950ba2e9026c

--- dist/src/plugins/compile-away-builder-components.d.ts
+++ dist/src/plugins/compile-away-builder-components.d.ts
@@ -1,6 +1,7 @@
 import { TraverseContext } from 'traverse';
 import { MitosisComponent } from '../types/mitosis-component';
 import { MitosisNode } from '../types/mitosis-node';
+import { Plugin } from '..';
 declare type CompileAwayComponentsMap = {
     [key: string]: (node: MitosisNode, context: TraverseContext, components: CompileAwayComponentsMap) => MitosisNode | void;
 };
@@ -10,9 +11,5 @@ declare type CompileAwayBuilderComponentsOptions = {
     omit?: string[];
 };
 export declare const compileAwayBuilderComponentsFromTree: (tree: MitosisNode | MitosisComponent, components: CompileAwayComponentsMap) => void;
-export declare const compileAwayBuilderComponents: (pluginOptions?: CompileAwayBuilderComponentsOptions) => (options?: any) => {
-    json: {
-        pre: (json: MitosisComponent) => void;
-    };
-};
+export declare const compileAwayBuilderComponents: (pluginOptions?: CompileAwayBuilderComponentsOptions) => Plugin;
 export {};
