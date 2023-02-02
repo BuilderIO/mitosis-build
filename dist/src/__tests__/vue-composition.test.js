d284ced13d59b712e5fd34dcd2f56de17768e7b4
Sahil H. Mobaidin <33333226+sahilmob@users.noreply.github.com>
2023-02-02 13:39:25 +0000

    c9e1699 handled function/method params (#1013)
    https://github.com/BuilderIO/mitosis/commit/c9e1699f2aa1b12b7e6a6d08ce9d1dc550a309d9

--- dist/src/__tests__/vue-composition.test.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var vue_1 = require("../generators/vue");
-var shared_1 = require("./shared");
-describe('Vue', function () {
-    (0, shared_1.runTestsForTarget)({ target: 'vue', generator: vue_1.componentToVue3, options: { api: 'composition' } });
-});
