99dde60bae8425933761bc50e1ba12a9ea57b559
SamuelOsborne <samu.osborne@gmail.com>
2023-02-08 18:59:46 +0000

    3efd67a Updated hooks.md with useStyle hook (#1025)
    https://github.com/BuilderIO/mitosis/commit/3efd67a62e588559715b9b348bb0a4ef4d363740

--- dist/src/__tests__/data/blocks/button.raw.lite.d.ts
+++
@@ -1,7 +0,0 @@
-export interface ButtonProps {
-    attributes?: any;
-    text?: string;
-    link?: string;
-    openLinkInNewTab?: boolean;
-}
-export default function Button(props: ButtonProps): import("@builder.io/mitosis/jsx-runtime").JSX.Element;
