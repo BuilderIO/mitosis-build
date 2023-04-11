20fd7f6505929d3efee6d4cfc62eeab8d824874f
Sami Jaber <jabersami@gmail.com>
2023-04-11 17:27:43 +0000

    f568a49 Fix: use TS option in Fiddle JSX (#1110)
    https://github.com/BuilderIO/mitosis/commit/f568a49ecb5b9930c4bcc8ae3ab5c602902ff4df

--- dist/src/__tests__/html.test.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var html_1 = require("../generators/html");
-var shared_1 = require("./shared");
-describe('Html', function () {
-    (0, shared_1.runTestsForTarget)({ options: {}, target: 'html', generator: html_1.componentToHtml });
-});
--- dist/src/__tests__/taro.test.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var taro_1 = require("../generators/taro");
-var shared_1 = require("./shared");
-describe('Taro', function () {
-    (0, shared_1.runTestsForTarget)({ options: {}, target: 'taro', generator: taro_1.componentToTaro });
-});
