4fa0bf2158c48ea6c437409284b217951e6384cd
Miško Hevery <misko@hevery.com>
2023-04-18 21:52:48 +0000

    17539ea fix(qwik): fix missing class attribute
    https://github.com/BuilderIO/mitosis/commit/17539eaa163f098d24eeadacb57f6d79b7574299

--- dist/src/parsers/builder/builder.js
+++ dist/src/parsers/builder/builder.js
@@ -331,6 +331,10 @@ var componentMappers = __assign(__assign({ Symbol: function (block, options) {
             css: { code: JSON.stringify(css) },
         }));
         var properties = __assign({}, block.properties);
+        if (block.id)
+            properties['block-id'] = block.id;
+        if (block.class)
+            properties['class'] = block.class;
         if (block.layerName) {
             properties.$name = block.layerName;
         }
