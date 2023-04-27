1adb93e959791b8e40b6f481e821332eea77a4f4
Sami Jaber <jabersami@gmail.com>
2023-04-27 22:00:41 +0000

    afdb648 Fix/context types react, release (#1148)
    https://github.com/BuilderIO/mitosis/commit/afdb648f30214e9bc1a3262096628831ee3616d5

--- dist/src/generators/context/react.d.ts
+++ dist/src/generators/context/react.d.ts
@@ -1,6 +1,7 @@
 import { MitosisContext } from '../../types/mitosis-context';
 declare type ContextToReactOptions = {
     format?: boolean;
+    typescript?: boolean;
 };
 export declare const contextToReact: (options?: ContextToReactOptions) => ({ context }: {
     context: MitosisContext;
