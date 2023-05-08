915b553b51c7aeec0bfa59f9e303d5d51bb8a935
Corbin Crutchley <crutchcorn@gmail.com>
2023-05-08 19:57:56 +0000

    a014bf2 fix: preact context generation imports from proper library (#1169)
    https://github.com/BuilderIO/mitosis/commit/a014bf2f341904ed344cb9c8b92a238c23e5552e

--- dist/src/generators/context/react.d.ts
+++ dist/src/generators/context/react.d.ts
@@ -1,6 +1,7 @@
 import { MitosisContext } from '../../types/mitosis-context';
 declare type ContextToReactOptions = {
     format?: boolean;
+    preact?: boolean;
     typescript?: boolean;
 };
 export declare const contextToReact: (options?: ContextToReactOptions) => ({ context }: {
--- dist/src/generators/context/react.js
+++ dist/src/generators/context/react.js
@@ -4,10 +4,10 @@ exports.contextToReact = void 0;
 var standalone_1 = require("prettier/standalone");
 var get_state_object_string_1 = require("../../helpers/get-state-object-string");
 var contextToReact = function (options) {
-    if (options === void 0) { options = { typescript: false }; }
+    if (options === void 0) { options = { typescript: false, preact: false }; }
     return function (_a) {
         var context = _a.context;
-        var str = "\n  import { createContext } from 'react';\n\n  export default createContext".concat(options.typescript ? '<any>' : '', "(").concat((0, get_state_object_string_1.stringifyContextValue)(context.value), ")\n  ");
+        var str = "\n  import { createContext } from '".concat(options.preact ? 'preact' : 'react', "';\n\n  export default createContext").concat(options.typescript ? '<any>' : '', "(").concat((0, get_state_object_string_1.stringifyContextValue)(context.value), ")\n  ");
         if (options.format !== false) {
             try {
                 str = (0, standalone_1.format)(str, {
