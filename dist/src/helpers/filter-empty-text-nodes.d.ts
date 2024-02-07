1d228b23b69fb4fa43a49c9990b68cd3409b8a0e
Sami Jaber <me@sami.website>
2024-02-07 20:31:59 +0000

    81e9944 Revert "Revert "Fix: redundant empty nodes (#1355)" (#1357)"
    https://github.com/BuilderIO/mitosis/commit/81e9944fe12d53bbcebb18170d71ae2b818c6b5d

--- dist/src/helpers/filter-empty-text-nodes.d.ts
+++ dist/src/helpers/filter-empty-text-nodes.d.ts
@@ -1,2 +1,3 @@
 import { MitosisNode } from '../types/mitosis-node';
+export declare const isEmptyTextNode: (node: MitosisNode) => boolean;
 export declare const filterEmptyTextNodes: (node: MitosisNode) => boolean;
--- dist/src/helpers/filter-empty-text-nodes.js
+++ dist/src/helpers/filter-empty-text-nodes.js
@@ -1,7 +1,9 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.filterEmptyTextNodes = void 0;
-var filterEmptyTextNodes = function (node) {
-    return !(typeof node.properties._text === 'string' && !node.properties._text.trim().length);
+exports.filterEmptyTextNodes = exports.isEmptyTextNode = void 0;
+var isEmptyTextNode = function (node) {
+    return typeof node.properties._text === 'string' && node.properties._text.trim().length === 0;
 };
+exports.isEmptyTextNode = isEmptyTextNode;
+var filterEmptyTextNodes = function (node) { return !(0, exports.isEmptyTextNode)(node); };
 exports.filterEmptyTextNodes = filterEmptyTextNodes;
